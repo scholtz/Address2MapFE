46d6bb01c3a6950825b6559af0945c6a6fa09614
Ludovit Scholtz <ludko2@gmail.com>
2022-10-08 15:08:52 +0200

    Last name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
 <template>
   <div class="home flex-grow-1 flex flex-row flex-auto">
     <h1>Welcome to address finder</h1>
+    <div><label>Last name:</label><input type="text" name="family_name" /></div>
   </div>
 </template>
 
