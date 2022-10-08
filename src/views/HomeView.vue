f26b8d27c75bef432aed3b33049906425d7b1b0b
Ludovit Scholtz <ludko2@gmail.com>
2022-10-08 15:12:24 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="home flex-grow-1 flex flex-row flex-auto">
+  <div class="home flex-grow-1 flex flex-column flex-auto">
     <h1>Welcome to address finder</h1>
-    <div><label>Last name:</label><input type="text" name="family_name" /></div>
+    <div class="my-2">
+      <label>Last name:</label> <input type="text" name="family_name" />
+    </div>
   </div>
 </template>
 
